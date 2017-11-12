8d5eef2f23849094f1aacfa1f49b836135184e8e
abhi-shek-gupta <abhishekgupta8751@gmail.com>
2017-11-12 19:26:23 +0530

    Task Done

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express'),
+http = require('http');
+const morgan = require('morgan');
+var PhoneNumber = require( 'awesome-phonenumber' );
+const bodyParser = require('body-parser');
+
+
+const hostname = 'localhost';
+const port = 3000;
+const app = express();
+
+// app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.get('/country/:phoneNumber', function(req, res){
+    // res.end("hello in get api");
+    var pn = new PhoneNumber(req.params.phoneNumber);
+    if(pn.a.valid){
+         var countryCode="+"+PhoneNumber.getCountryCodeForRegionCode(pn.a.regionCode);
+         console.log("country code is : +"+countryCode);
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({"country":countryCode})
+        
+    }
+    else{
+        res.statusCode = 400;
+        console.log("Invalid formate");
+        res.json({"errorMessage":"Invalid phone number"})
+    }
+    
+  });
+
+//for serving static pages
+app.use((req, res) => {
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end("connected to server");
+    
+    });
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+console.log(`Server running at http://${hostname}:${port}/`);
+});
